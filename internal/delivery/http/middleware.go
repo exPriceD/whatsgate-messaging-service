677ab5cb032b59e4067854e25e60dbb9f712e0d9
Maxim Bogdanov <lolerpro4@gmail.com>
2025-06-25 03:45:28 +0300

    [chore]: using error handler mw

--- internal/delivery/http/middleware.go
+++ internal/delivery/http/middleware.go
@@ -7,6 +7,7 @@ import (
 // setupMiddleware настраивает middleware для сервера.
 func (s *Server) setupMiddleware() {
 	s.engine.Use(middleware.Recovery(s.logger))
+	s.engine.Use(middleware.ErrorHandler(s.logger))
 	s.engine.Use(middleware.CORSMiddleware(s.config.CORS))
 	s.engine.Use(middleware.Logging(s.logger))
 }
