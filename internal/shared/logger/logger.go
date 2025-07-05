40aaf1301c3e1dac39c49ef6508f41d88542f26d
Maxim Bogdanov <lolerpro4@gmail.com>
2025-07-05 22:28:36 +0300

    [feet]: added logger contract in global package

---
+++ internal/shared/logger/logger.go
@@ -0,0 +1,9 @@
+package logger
+
+type Logger interface {
+	Info(msg string, fields ...any)
+	Warn(msg string, fields ...any)
+	Error(msg string, fields ...any)
+	Debug(msg string, fields ...any)
+	With(fields ...any) Logger
+}
