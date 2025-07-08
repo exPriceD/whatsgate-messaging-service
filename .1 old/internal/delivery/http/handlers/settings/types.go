21f04ce130aff73b9baf63cefedf251544abad11
Maxim Bogdanov <lolerpro4@gmail.com>
2025-07-08 14:18:43 +0300

    deleted

--- .1 old/internal/delivery/http/handlers/settings/types.go
+++
@@ -1,8 +0,0 @@
-package settings
-
-// WhatsGateSettings представляет настройки WhatGate API.
-type WhatsGateSettings struct {
-	WhatsappID string `json:"whatsapp_id" binding:"required" example:"your_whatsapp_id"`
-	APIKey     string `json:"api_key" binding:"required" example:"your_api_key"`
-	BaseURL    string `json:"base_url" example:"https://whatsgate.ru/api/v1"`
-}
